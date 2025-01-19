d16b1fe3a5e799d243676c4ebb7bdc618cebf881
Rahul pratap singh <rahulprataps386@gmail.com>
2025-01-19 19:33:42 +0530

    updating url

--- client/src/api/user/QRapi.js
+++ client/src/api/user/QRapi.js
@@ -2,7 +2,7 @@ const handleDecode = async (result) => {
   console.log("QR Code scanned:", result);
   setScannedData(result);
   try {
-    const response = await fetch(`http://localhost:3000/api/items/${result}`);
+    const response = await fetch(`https://be-fit-server.onrender.com/api/items/${result}`);
     const data = await response.json();
     // Handle the fetched data as needed
   } catch (apiError) {
